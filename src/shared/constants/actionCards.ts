d854345e5239100e4d924a5b660b1b57b53abf06
Mahdiyar <mahyar_moghadam11@yahoo.ca>
2026-09-22 16:57:11 +0330

    income - aid fixed

--- src/shared/constants/actionCards.ts
+++ src/shared/constants/actionCards.ts
@@ -1,11 +1,11 @@
 
 export const ACTION_CARD_DATA: Record<string, { image: string; title: string; description: string }> = {
-  income: { image: "/assets/images/cards/income.jpg", title: "درآمد", description: "یک سکه از بانک بردار" },
-  foreign_aid: { image: "/assets/images/cards/aid.jpg", title: "کمک خارجی", description: "دو سکه از بانک بردار (قابل بلاک)" },
-  coup: { image: "/assets/images/cards/coup.png", title: "کودتا", description: "با ۷ سکه یک کارت حریف رو حذف کن" },
-  tax: { image: "/assets/images/cards/minimal/duke.png", title: "مالیات (بزرگ‌زاده)", description: "سه سکه از بانک بردار" },
-  assassinate: { image: "/assets/images/cards/assassin1.jpg", title: "ترور (قاتل)", description: "با ۳ سکه یک کارت حریف رو حذف کن" },
-  steal: { image: "/assets/images/cards/minimal/captain.png", title: "دزدی (فرمانده)", description: "دو سکه از یک حریف بدزد" },
+  income: { image: "/assets/images/cards/minimal/income.png", title: "درآمد", description: "یک سکه از بانک بردار" },
+  foreign_aid: { image: "/assets/images/cards/minimal/aid.png", title: "کمک خارجی", description: "دو سکه از بانک بردار (قابل بلاک)" },
+  coup: { image: "/assets/images/cards/coup.png", title: "کودتا", description: "با ۷ سکه کودتا کن" },
+  tax: { image: "/assets/images/cards/minimal/duke.png", title: "مالیات (بزرگ‌زاده)", description: "3سکه از بانک بردار" },
+  assassinate: { image: "/assets/images/cards/assassin1.jpg", title: "ترور (قاتل)", description: "با ۳ سکه یک قتل انجام بده" },
+  steal: { image: "/assets/images/cards/minimal/captain.png", title: "دزدی (فرمانده)", description: "2 سکه از یک حریف بدزد" },
   exchange: { image: "/assets/images/cards/minimal/ambassador.png", title: "تعویض (سفیر)", description: "کارت‌هاتو با دسته عوض کن" },
   contessa_self: { image: "/assets/images/cards/minimal/contessa.png", title: "تعویض خودم (بازرس)", description: "یکی از کارت‌هاتو عوض کن" },
   contessa_other: { image: "/assets/images/cards/minimal/contessa.png", title: "اجبار به تعویض (بازرس)", description: "حریف رو مجبور به تعویض یه کارت کن" },
