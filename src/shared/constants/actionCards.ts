2017a10a826ef8a893263e804af1bdd7196c3b4b
Mahdiyar <mahyar_moghadam11@yahoo.ca>
2026-09-23 04:13:12 +0330

    assassin card

--- src/shared/constants/actionCards.ts
+++ src/shared/constants/actionCards.ts
@@ -4,7 +4,7 @@ export const ACTION_CARD_DATA: Record<string, { image: string; title: string; de
   foreign_aid: { image: "/assets/images/cards/minimal/aid.png", title: "کمک خارجی", description: "دو سکه از بانک بردار (قابل بلاک)" },
   coup: { image: "/assets/images/cards/minimal/coup.png", title: "کودتا", description: "با ۷ سکه کودتا کن" },
   tax: { image: "/assets/images/cards/minimal/duke.png", title: "مالیات (بزرگ‌زاده)", description: "3سکه از بانک بردار" },
-  assassinate: { image: "/assets/images/cards/assassin1.jpg", title: "ترور (قاتل)", description: "با ۳ سکه یک قتل انجام بده" },
+  assassinate: { image: "/assets/images/cards/minimal/assassin.jpg", title: "ترور (قاتل)", description: "با ۳ سکه یک قتل انجام بده" },
   steal: { image: "/assets/images/cards/minimal/captain.png", title: "دزدی (فرمانده)", description: "2 سکه از یک حریف بدزد" },
   exchange: { image: "/assets/images/cards/minimal/ambassador.png", title: "تعویض (سفیر)", description: "کارت‌هاتو با دسته عوض کن" },
   contessa_self: { image: "/assets/images/cards/minimal/contessa.png", title: "تعویض خودم (بازرس)", description: "یکی از کارت‌هاتو عوض کن" },
